250ad12c711744d5ff8cbbbd9172c77c3600496c
Yegor Shytikov <egorshitikov@gmail.com>
2021-07-26 03:15:40 +0000

    more examples

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 var initModels = require("./Models/init-models");
 
 const sequelize = new Sequelize(
-    'magento24_nodegento',
+    'magento',
     'root',
     '',
     {
